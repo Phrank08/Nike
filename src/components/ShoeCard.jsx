35822f855187474a00e5fc73b9c4d0a8fdd814fb
Phrank08 <frankokey469@gmail.com>
2025-03-31 07:29:04 -0700

    Enhance ShoeCard component with click handling and dynamic border styling

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,6 +1,29 @@
-const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImage}) => {
+const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImg}) => {
+
+  const handleClick = () => {
+    if(bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe)
+    }
+  }
+
   return (
-    <div>ShoeCard</div>
+    <div className={`border-2
+      rounded-xl
+      ${bigShoeImg === imgURL
+        ? 'border-coral-red'
+        : 'border-transparent'
+      } cursor-pointer max-sm:flex-1
+      `}
+      onclick={handleClick}
+      >
+
+      <div>
+        <img
+          src={imgURL.thumbnail}
+          />
+      </div>
+
+    </div>
   )
 }
 
