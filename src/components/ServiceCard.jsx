47aa1d547ebeeef2df61453c61313fbe8eafe4d8
Phrank08 <frankokey469@gmail.com>
2025-04-08 11:33:31 -0700

    Refactor ServiceCard component to accept props for image URL, label, and subtext; enhance layout with styling

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,14 @@
-const ServiceCard = () => {
+const ServiceCard = ({imgURL, label, subtext}) => {
   return (
-    <div>ServiceCard</div>
+    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
+      <div className="w-11 h-11 justify-center items-center bg-coral-red">
+        <img 
+        src={imgURL} alt={label} 
+        width={24} height={24}/>
+
+      </div>
+
+    </div>
   )
 }
 
