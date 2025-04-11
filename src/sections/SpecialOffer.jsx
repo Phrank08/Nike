77ed2ddc5914068c24bdbee6c80fb4f37d17c2d5
Phrank08 <frankokey469@gmail.com>
2025-04-11 09:07:31 -0700

    Enhance Button component with customizable styles; implement CustomerReviews section with ReviewCard; update SpecialOffer section with new content and buttons

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,5 +1,6 @@
 import { offer } from '../assets/images'
 import Button from '../components/Button'
+import { arrowRight } from '../assets/icons'
 
 const SpecialOffer = () => {
   return (
@@ -10,24 +11,29 @@ const SpecialOffer = () => {
         height={687}
         className="object-contain w-full"/>
       </div>
-      
+
       <div className="flex flex-1 flex-col">  
         <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
-          We Provide You <span className="text-coral-red ">
-            Super
-          </span> <span className="text-coral-red ">
-            Quality
-          </span> Shoes
+          <span className="text-coral-red ">
+            Special
+          </span> Offer
         </h2>
-          <p className="mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, our meticulosly crafted fiitwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.
+          <p className="mt-4 lg:max-w-lg info-text">Embark on a shopping journey that redefines your experience with unbeatable deals. From premire selections to incredible savings, we are committed to bringing you the best in quality and value.
           </p>
-  
+          
+
           <p className="mt-6 lg:max-w-lg info-text">
-            Our dedication to excellence is reflected in every detail, from the selection of materials to the precision of craftsmanship.
+            Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.
           </p>
   
-          <div className="mt-11">
-            <Button label="View details"/>
+          <div className="mt-11 flex flex-wrap gap-4">
+            <Button label="Shop Now"
+            iconURL={arrowRight}/>
+
+            <Button label="Learn More"
+              backgroundColor="bg-white"
+              borderColor="border-slate-gray"
+              textColor="text-slate-gray"/>
           </div>
   
         </div>
